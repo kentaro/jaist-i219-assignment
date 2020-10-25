a012c043db9e18f1d90348b43364d6b35037281c
Kentaro Kuribayashi <kentarok@gmail.com>
2020-10-25 19:54:10 +0900

    renamed

--- exercise2/ExpParseTree.java
+++
@@ -1,3 +0,0 @@
-public interface ExpParseTree {
-    int calculate();
-}
--- exercise5/SSuperB.java
+++
@@ -1,9 +0,0 @@
-class SSuperB {
-    public void m2(SuperB x, SuperB y) {
-        System.out.println("m2(SuperB x, SuperB y) in SSuperB;");
-    }
-
-    public void m2(SSuperB x, SSuperB y) {
-        System.out.println("m2(SSuperB x, SSuperB y) in SSuperB;");
-    }
-}
