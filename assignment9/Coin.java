1bf55a59cedc9ec96116fac0173fd58833673b6b
Kentaro Kuribayashi <kentarok@gmail.com>
2020-11-13 00:56:45 +0900

    added programs

---
+++ assignment9/Coin.java
@@ -0,0 +1,11 @@
+/**
+ * 栗林健太郎
+ * 2030006
+ * 2020年11月12日
+ */
+
+public enum Coin {
+    TEN,
+    FIFTY,
+    HUNDRED;
+}
---
+++ assignment9/Item.java
@@ -0,0 +1,21 @@
+/**
+ * 栗林健太郎
+ * 2030006
+ * 2020年11月12日
+ */
+
+public enum Item {
+    MILK(40),
+    JUICE(50),
+    COFFEE(120);
+
+    private final int cost;
+
+    private Item(int c) {
+        cost = c;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+}
